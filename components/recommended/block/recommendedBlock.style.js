d8a3b01f6484ebf37a3046b553161aebcdb3c81b
n-freman <smiron1703@gmail.com>
2023-05-07 17:16:22 +0500

    edit recommended block title

--- components/recommended/block/recommendedBlock.style.js
+++ components/recommended/block/recommendedBlock.style.js
@@ -14,7 +14,8 @@ const styles = StyleSheet.create({
         fontFamily: "BarlowBold",
         fontSize: scale(40),
         lineHeight: verticalScale(48),
-        color: COLORS.white
+        color: COLORS.white,
+        marginBottom: verticalScale(15)
     },
 
 })
